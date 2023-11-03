c87512fdd5998dd5ee8abb98f58f11097f0d29a3
fzhcary <frank.zhu.mingyu@gmail.com>
2023-11-03 16:12:44 -0400

    refactor routes

--- src/app/main.py
+++ src/app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
+from app.api import ping
+
 app = FastAPI()
 
+# wire ping route to main app
+app.include_router(ping.router)
 
-@app.get("/ping")
-def pong():
-    return {"ping": "pong!"}
