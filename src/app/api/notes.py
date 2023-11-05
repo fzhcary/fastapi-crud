07bb8821d67e940fc5d4f2651e43998632667e0d
fzhcary <frank.zhu.mingyu@gmail.com>
2023-11-04 22:36:14 -0400

    add extra validation

--- src/app/api/notes.py
+++ src/app/api/notes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Path
 from typing import List
 from app.api import crud
 from app.api.models import NoteDB, NoteSchema
@@ -19,7 +19,7 @@ async def create_note(payload: NoteSchema):
 
 
 @router.get("/{id}/", response_model=NoteDB)
-async def read_note(id: int):
+async def read_note(id: int = Path(..., gt=0),):
     note = await crud.get(id)
     if not note:
         raise HTTPException(status_code=404, detail="Note not found")
@@ -32,7 +32,7 @@ async def read_all_notes():
 
 
 @router.put("/{id}/", response_model=NoteDB)
-async def update_note(id: int, payload: NoteSchema):
+async def update_note(payload: NoteSchema, id: int = Path(..., gt=0),):
     note = await crud.get(id)
     if not note:
         raise HTTPException(status_code=404, detail="Note not found")
